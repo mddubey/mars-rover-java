12c513a351b7833cfef1513a41c6efc39c71233d
MdDubey <md@whatever.com>
2014-05-05 11:41:12 +0530

    creating plateau method written in factory

--- src/com/tw/mritunjd/marserover/factory/MarsRoverFactory.java
+++ src/com/tw/mritunjd/marserover/factory/MarsRoverFactory.java
@@ -1,10 +1,15 @@
 package com.tw.mritunjd.marserover.factory;
 
+import com.tw.mritunjd.marserover.Plateau;
 import com.tw.mritunjd.marserover.Rover;
 
 public class MarsRoverFactory {
 
     public Rover createRover(int position_x_axis, int position_y_axis, Character directionSymbol) {
         return new Rover(position_x_axis, position_y_axis, directionSymbol);
+    }
+
+    public Plateau createPlateau(int max_x_axis, int max_y_axis, MarsRoverFactory factory) {
+        return new Plateau(max_x_axis, max_y_axis, factory);
     }
 }
