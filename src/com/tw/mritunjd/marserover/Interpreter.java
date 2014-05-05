5522957f68272329459e9071f10a9355ffa514a1
MdDubey <md@whatever.com>
2014-05-05 11:45:32 +0530

    main added to all the functionalities , finished first step

---
+++ src/com/tw/mritunjd/marserover/Interpreter.java
@@ -0,0 +1,51 @@
+package com.tw.mritunjd.marserover;
+
+import com.tw.mritunjd.marserover.directions.SOUTH;
+import com.tw.mritunjd.marserover.factory.MarsRoverFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Interpreter {
+    private Plateau plateau;
+    private MarsRoverFactory factory;
+
+    public Interpreter(MarsRoverFactory factory) {
+        this.factory = factory;
+    }
+
+    public List<String> getInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inputs for Plateau and rovers:--");
+        List<String> inputs = new ArrayList<>();
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            if (line.equalsIgnoreCase("end"))
+                break;
+            inputs.add(line);
+        }
+        return inputs;
+    }
+
+    public void performOperationOnInput(List<String> inputs) {
+        String[] upperRightCoordinates = inputs.get(0).split(" ");
+        int max_x_axis = Integer.parseInt(upperRightCoordinates[0]);
+        int max_y_axis = Integer.parseInt(upperRightCoordinates[1]);
+        plateau = this.factory.createPlateau(max_x_axis, max_y_axis, this.factory);
+        int index = 1;
+        System.out.println("Location after exploration:--");
+        while (index < inputs.size()) {
+            String[] initRoverInput = inputs.get(index++).split(" ");
+            String movementInstructions = inputs.get(index++);
+            int position_x_axis = Integer.parseInt(initRoverInput[0]);
+            int position_y_axis = Integer.parseInt(initRoverInput[1]);
+            Character directionSymbol = initRoverInput[2].charAt(0);
+            plateau.initRover(position_x_axis, position_y_axis, directionSymbol);
+            plateau.explorePlateau(movementInstructions);
+            String result = plateau.getRoverLocation();
+            System.out.println(result);
+        }
+    }
+
+}
--- src/com/tw/mritunjd/marserover/MarsRover.java
+++ src/com/tw/mritunjd/marserover/MarsRover.java
@@ -1,7 +1,13 @@
 package com.tw.mritunjd.marserover;
 
+import com.tw.mritunjd.marserover.factory.MarsRoverFactory;
+
+import java.util.List;
+
 public class MarsRover {
     public static void main(String[] args) {
-        System.out.println("i am here");
+        Interpreter interpreter = new Interpreter(new MarsRoverFactory());
+        List<String> inputs = interpreter.getInput();
+        interpreter.performOperationOnInput(inputs);
     }
 }
